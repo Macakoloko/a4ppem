01b6523ff1c873634768bfde4521baabfd2daae0
Gabriel Ferreira <gabmondie@gmail.com>
2025-03-25 12:10:31 +0000

    Adiciona arquivo de barril para exportar todos os componentes

---
+++ components/index.js
@@ -0,0 +1,34 @@
+// Exportações de componentes principais
+export { default as ThemeProvider } from './theme-provider';
+export { default as ModeToggle } from './mode-toggle';
+export { default as AuthForm } from './auth-form';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as Auth } from './Auth';
+export { default as MainNav } from './main-nav';
+export { default as Navigation } from './navigation';
+export { default as MobileNavigation } from './mobile-navigation';
+
+// Exportações de componentes de UI
+export { Button } from './ui/button';
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './ui/card';
+export { Input } from './ui/input';
+export { Label } from './ui/label';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
+export { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+export { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select';
+export { Separator } from './ui/separator';
+export { Switch } from './ui/switch';
+export { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from './ui/table';
+
+// Componentes de formulários
+export { default as ClientForm } from './client-form';
+export { default as ClientList } from './client-list';
+export { default as ProductForm } from './product-form';
+export { default as ProductList } from './product-list';
+export { default as ServiceForm } from './service-form';
+export { default as ServiceList } from './service-list';
+export { default as ProfessionalForm } from './professional-form';
+export { default as ProfessionalList } from './professional-list';
+export { default as AppointmentForm } from './appointment-form';
+export { default as AppointmentList } from './appointment-list';
+export { default as AppointmentCalendar } from './appointment-calendar'; 
